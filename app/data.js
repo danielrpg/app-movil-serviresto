223429258090ee3f3c223c80fc6d6e2b84a7d59a
danielrpg <daniel.fernandezgp@gmail.com>
2013-08-11 21:29:27 -0400

    Modificacion del select de servicios desde el sql

--- app/data.js
+++ app/data.js
@@ -31,7 +31,17 @@ App.Data =(function(lng, app, undefined){
 	/** Metodo que lista los servicios desde la base de datos**/
 	var getListaServicios = function(){
 		db.transaction(function(sql){
-			sql.executeSql();
+			sql.executeSql('SELECT * FROM servicios', [], function (sql, results) {
+				var len = results.rows.length, i;
+				var json_clientes ="";
+				var data = [];
+				for (i = 0; i < len; i++){
+      				var servicio = {"id_servicio":results.rows.item(i).id_servicio,"codigo_servicio":results.rows.item(i).codigo_servicio, "nombre_servicio":results.rows.item(i).nombre_servicio};
+      				data.push(servicio);
+   				}
+   				json_servicios=JSON.stringify({'servicios':data});
+   				app.Views.showListaServicios($.parseJSON(json_servicios));
+			}, insertError);
 		});
 	};
 	/** Este es el metodo cuando se a ejecutado la consulta de manera correcta **/
